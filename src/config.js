d3ff246bbd85c059d2cda48b58a255932767cbff
apnneji <apnneji@gmail.com>
2025-07-29 23:27:06 +0300

    httpsfree

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Global configuration
-export const isWeb = true; // Change to false for local development
+export const isWeb = false; // Change to false for local development
 
 // API credentials
 export const webapiusername = '11253820';
@@ -7,7 +7,7 @@ export const webapipassword = '60-dayfreetrial';
 
 // API URLs
 export const LOCAL_API_BASE = 'http://localhost:5001/api/user';
-export const WEB_API_BASE = 'http://apnneji-001-site1.ktempurl.com/api/User';
+export const WEB_API_BASE = 'https://apnneji-001-site1.ktempurl.com/api/User';
 
 // Helper function to get the appropriate API URL
 export const getApiUrl = (endpoint) => {
